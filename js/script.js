93bbc29346e1a4507101b12fafdaeb4caee8fc3f
nicolasmoura23 <thenicou@gmail.com>
2020-08-20 20:09:54 -0300

    Adicionar evento de busca

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //selecionar intens a serem ativos
 const carregar = document.querySelector("[data-animacao='caixa']");
-const cards = document.querySelectorAll("[data-temperatura='card']")
-
+const cards = document.querySelectorAll("[data-temperatura='card']");
+const erro = document.querySelector("[data-temperatura='erro']");
+//Selecionar select para controle
+const select = document.querySelector("#cidades")
 // ativa um item
 function ativarItem(item, classe = "ativo") {
     item.classList.add(classe); 
@@ -17,17 +19,42 @@ function ativarItens(itens, classe) {
 
 //desativa um item
 function desativarItem(item, classe = "ativo") {
-    item.classList.remove(classe); 
+    item.classList.remove(classe, classe); 
 }
 
 //desativa uma lista de itens
 function desativarItens(itens, classe) {
     for (let i = 0; i < itens.length; i++){
-        desativarItem(itens[i]);
+        desativarItem(itens[i], classe);
     }
 }
 
-ativarItens(cards);
-ativarItem(carregar)
-dasativarItem(carregar);
-desativarItens(cards);
+
+// Busca os dados na API
+function buscarDados(evento) {
+    const cidade = evento.currentTarget.value;
+
+    // Inicia a animação de busca e limpa e os dados
+    desativarItem(erro);
+    desativarItens(cards);
+    ativarItem(carregar);
+
+    // Faz a requisição
+    const requisicao = fetch(`https://goweather.herokuapp.com/weather/${cidade}`);
+    requisicao.then( (resposta) => resposta.json)
+    .then((json) => {
+        // Retorno da requisição
+        ativarItens(cards);
+    })
+    .catch((e) => {
+        // Erros na busca
+        ativarItem(erro);
+    })
+    .finally((r) => {
+        // Encerramento da busca
+        desativarItem(carregar);
+    });
+}
+
+// Adicionar o evento para manipular dados
+select.addEventListener("change", buscarDados);
