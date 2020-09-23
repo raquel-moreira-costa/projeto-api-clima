dc24c17ca7fbc33b36ded4fec63ae1c8c251d792
Paulo <pauloh16.pdr@gmail.com>
2020-09-23 18:19:45 -0300

    Ajustar pagina deletar

---
+++ js/deletar.js
@@ -0,0 +1,7 @@
+// Selecionar select para controle
+const select = document.querySelector("#cidades");
+
+// Regasta o útlimo valor selecionado
+window.addEventListener("load", () => {
+    carregarOpcoes(select);
+});
