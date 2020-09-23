582545f06dbfa4b9ce36f2786ae1621027bc8c9f
Paulo <pauloh16.pdr@gmail.com>
2020-09-23 18:40:48 -0300

    Adicionar script em deletar

--- js/deletar.js
+++ js/deletar.js
@@ -5,3 +5,20 @@ const select = document.querySelector("#cidades");
 window.addEventListener("load", () => {
     carregarOpcoes(select);
 });
+
+// Seleciona o botão
+const btn = document.querySelector("#deletar button");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // Pegando o valor do select
+    const valor = select.value;
+
+    // Valida se algum valor foi selecionado
+    if (valor !== "") {
+        // Faz a requisição com o verbo delete
+        fetch(`http://localhost:3000/temperaturas/${valor}`, {method: "DELETE"})
+        .catch((erro) => {console.log(erro)});
+    }
+});
